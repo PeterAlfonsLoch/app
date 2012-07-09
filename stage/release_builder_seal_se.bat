df23c24591c5f232b21cb05fd5ac41810d517cd2
ca2 <camilo@ca2.email>
2012-07-09 05:42:40 +0000

    ﻿-c1-production -c2-producer -t12n-producing -mmmi- 2012-07-09 05-41-00 app:SVN4877M, app-cidadedecuritiba:SVN65, app-core:SVN522, app-frontpage:SVN97, app-sysutils:SVN76, app-veriwell:SVN463 - // CarlosGustavoCecynLundgren1984+kaarurosu日歩路主 // <==! Thanks and dedicated to &lt; < // Thanks to and in honor of God > &gt; ==> // for ca2 and 5!! // you(*) and me(*) and God(***) make 5... #define THIS_PRODUCT_VERSION "2012-07-09 05-41-00 app:SVN4877M, app-cidadedecuritiba:SVN65, app-core:SVN522, app-frontpage:SVN97, app-sysutils:SVN76, app-veriwell:SVN463\0" #define THIS_FILE_VERSION "2012-07-09 05-41-00 app:SVN4877M, app-cidadedecuritiba:SVN65, app-core:SVN522, app-frontpage:SVN97, app-sysutils:SVN76, app-veriwell:SVN463\0" #define __THIS_PRODUCT_VERSION 2012,0709,0541,00 #define __THIS_FILE_VERSION 2012,0709,0541,00

--- stage/release_builder_seal_se.bat
+++ stage/release_builder_seal_se.bat
@@ -1,6 +1,6 @@
-type C:\bergedge\app\stage\build_folder_pp_comment_se.txt > V:\basis\app\stage\release_builder_seal_se.txt
+type G:\bergedge\app\stage\build_folder_pp_comment_se.txt > V:\basis\app\stage\release_builder_seal_se.txt
 date /T >> V:\basis\app\stage\release_builder_seal_se.txt
 time /T >> V:\basis\app\stage\release_builder_seal_se.txt
-type C:\bergedge\app\stage\build_machine_pp_comment_se.txt >> V:\basis\app\stage\release_builder_seal_se.txt
+type G:\bergedge\app\stage\build_machine_pp_comment_se.txt >> V:\basis\app\stage\release_builder_seal_se.txt
 type G:\cecyn1.at.hotmail.com\ccvotagus\dedicaverse.txt >> V:\basis\app\stage\release_builder_seal_se.txt
 svn commit --force-log --encoding utf-8 --file=V:\basis\app\stage\release_builder_seal_se.txt V:\basis\app\stage
