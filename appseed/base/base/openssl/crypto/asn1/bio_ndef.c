65436bbfc2a471a2b86da4d1a1a149b918a2b42a
ca2 <camilo@ca2.email>
2014-02-03 01:10:43 +0000

    SmartSVN help!! Use the license you were assigned!! Thank you God!! At inha Ubuntu VM Workstation!!

--- appseed/base/base/openssl/crypto/asn1/bio_ndef.c
+++ appseed/base/base/openssl/crypto/asn1/bio_ndef.c
@@ -10,7 +10,7 @@
  * are met:
  *
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
+ *    notice, this list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
@@ -57,7 +57,7 @@
 #include <openssl/bio.h>
 #include <openssl/err.h>
 
-#include <stdio.h>
+////#include <stdio.h>
 
 /* Experimental NDEF ASN1 BIO support routines */
 
