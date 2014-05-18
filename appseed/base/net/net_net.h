8006b17de76a827e027f6ca6b74cefaf49fad6f2
ca2 <camilo@ca2.email>
2014-05-18 04:00:11 +0000

    Release Files Distributed to All Servers!! Thank you God!! Now SP Enterprise OVH servers!!

--- appseed/base/net/net_net.h
+++ appseed/base/net/net_net.h
@@ -15,6 +15,8 @@ CLASS_DECL_BASE string c_inet_ntop(int32_t af, const void *src);
 
 CLASS_DECL_BASE string c_gethostbyname(const char * hostname);
 
+CLASS_DECL_BASE string get_file_extension_mime_type(const string & strExtension);
+
 
 #include "net_byte_order.h"
 
