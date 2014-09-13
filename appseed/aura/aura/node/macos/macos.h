b69abbedf4aa29225e4cc90bc32753c49ab5dbda
ca2 <camilo@ca2.email>
2014-09-13 17:06:39 +0000

    from Z:\eco folder at inha from Curitiba

--- appseed/aura/aura/node/macos/macos.h
+++ appseed/aura/aura/node/macos/macos.h
@@ -103,7 +103,46 @@ int32_t CLASS_DECL_mac __mac_main(int32_t argc, char * argv[]);
 
 CLASS_DECL_mac void vfxThrowFileException(::aura::application * papp, int32_t cause, LONG lOsError, const char * lpszFileName = NULL);
 
+#pragma once
 
 
-#include "mac_internal.h"
+// from wincore.cpp
+extern CLASS_DECL_mac const char _afxWnd[];           // simple child windows/controls
+extern CLASS_DECL_mac const char _afxWndControlBar[]; // controls with gray backgrounds
+extern CLASS_DECL_mac const char _afxWndMDIFrame[];
+extern CLASS_DECL_mac const char _afxWndFrameOrView[];
+extern CLASS_DECL_mac const char _afxWndOleControl[];
 
+
+/////////////////////////////////////////////////////////////////////////////
+// Special helpers
+
+CLASS_DECL_mac void AfxCancelModes(oswindow hWndRcvr);
+CLASS_DECL_mac WINBOOL AfxHelpEnabled();  // determine if ID_HELP handler exists
+CLASS_DECL_mac WINBOOL AfxCustomLogFont(UINT nIDS,LOGFONT* pLogFont);
+CLASS_DECL_mac WINBOOL AfxGetPropSheetFont(string & strFace,WORD& wSize,WINBOOL bWizard);
+
+CLASS_DECL_mac WINBOOL _AfxIsComboBoxControl(oswindow hWnd,UINT nStyle);
+CLASS_DECL_mac WINBOOL _AfxCheckCenterDialog(const char * lpszResource);
+CLASS_DECL_mac WINBOOL _AfxCompareClassName(oswindow hWnd,const char * lpszClassName);
+CLASS_DECL_mac oswindow _AfxChildWindowFromPoint(oswindow,POINT);
+
+// for determining version of COMCTL32.DLL
+#define VERSION_WIN4    MAKELONG(0, 4)
+#define VERSION_IE3             MAKELONG(70, 4)
+#define VERSION_IE4             MAKELONG(71, 4)
+#define VERSION_IE401   MAKELONG(72, 4)
+#define VERSION_6      MAKELONG(0, 6)
+extern int _afxComCtlVersion;
+DWORD _AfxGetComCtlVersion();
+
+
+#pragma once
+
+
+#include "mac_dir.h"
+#include "mac_file_system.h"
+#include "mac_file.h"
+#include "mac_shell.h"
+#include "mac_stdio_file.h"
+#include "mac_file_set.h"
