b68a865f91d783107694f533627f99c13de642f6
cecyn1 <cecyn1@hotmail.com>
2015-02-09 11:44:54 +0000

    Merged revision(s) 14306-14307 from netnode: Thank you and Sorry Mummi!! God please let Maxihost effectively help us especially us as us Brazil and us Camilo, Mummi and Carlos => ca2 grows, Multiverse love!!

--- appseed/aura/primitive/collection/collection_sort.cpp
+++ appseed/aura/primitive/collection/collection_sort.cpp
@@ -117,8 +117,8 @@ namespace  sort
       index iSize,
       ::array < ARG_COMPARE_FUNCTION, ARG_COMPARE_FUNCTION > & comparefna,
       ::array < ARG_SWAP_FUNCTION, ARG_SWAP_FUNCTION > & swapfna,
-      ::raw_ptr_array < void * > & comparearga,
-      ::raw_ptr_array < void * > & swaparga)
+      void_ptra & comparearga,
+      void_ptra & swaparga)
    {
       
       index_array stackLowerBound;
