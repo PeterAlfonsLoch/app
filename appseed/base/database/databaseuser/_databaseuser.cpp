655313c6c6d34ed183cf3d723f17c1986e0d11b0
camilox_porting_to_ios <camilox_porting_to_ios@ca2.cc>
2015-08-03 17:55:10 +0000

    Mac OS contribution!! Apple contribution!! iTunes play along contribution!! Security contribution!!
    Thank you Carlinhos!!
    
    Thank you Mummi!!
    
    Thank you Bilbo and Lucinho!!

---
+++ appseed/base/database/databaseuser/_databaseuser.cpp
@@ -0,0 +1,8 @@
+#include "framework.h"
+#include "core/user/user/user.h"
+#include "core/filesystem/filemanager/filemanager.h"
+
+
+// thank you (casey) for the amalgamation conception reiteration (tip from sqlite amalgamation first contact...)
+#include "database_key_mesh_data.cpp"
+#include "database_mesh_data.cpp"
