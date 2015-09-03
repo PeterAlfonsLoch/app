6302278ca488281bed77bfcc2ef02fc710107565
cecyn1 <cecyn1@hotmail.com>
2015-09-03 01:50:41 +0000

    CGCL!!

--- axis/openssl/ssl/dtls1.h
+++ axis/openssl/ssl/dtls1.h
@@ -70,7 +70,7 @@
 #ifdef OPENSSL_SYS_WIN32
 /* Needed for struct timeval */
 #include <windows.h>
-#include <winsock2.h>
+//#include <winsock2.h>
 #elif defined(OPENSSL_SYS_NETWARE) && !defined(_WINSOCK2API_)
 #include <sys/timeval.h>
 #else
