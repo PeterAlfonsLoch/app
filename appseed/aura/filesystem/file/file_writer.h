81da09724b13383fb3f7a96017df66815dcfc323
ca2 <camilo@ca2.email>
2016-02-09 02:28:35 +0000

    LCTV contribution!! danyguag, blankbanshee and robdante contribution!!
    from all new bilbo-ubuntu
    Thank you Mummi!
    Thank you Carlinhos!!

--- appseed/aura/filesystem/file/file_writer.h
+++ appseed/aura/filesystem/file/file_writer.h
@@ -15,16 +15,16 @@ namespace file
    {
    public:
 
-      
+
       writer();
       virtual ~writer();
 
-      
+
       virtual void write(const void *lpBuf, memory_size_t nCount);
       virtual void write(const void *lpBuf, memory_size_t nCount, memory_size_t * dwWritten);
 
-      virtual void transfer_from(reader & reader, memory_size_t uiBufferSize = 1024 * 1024);
-      virtual void transfer_from_begin(reader & reader, memory_size_t uiBufSize = 1024 * 1024);
+      virtual void transfer_from(reader & reader, memory_size_t uiBufferSize = 16 * 1024);
+      virtual void transfer_from_begin(reader & reader, memory_size_t uiBufSize = 16 * 1024);
 
       virtual void close();
 
@@ -36,7 +36,7 @@ namespace file
 
    typedef writer writable;
 
-   
+
    CLASS_DECL_AURA HRESULT write(writer * pwriter, const void * data, memory_size_t size);
 
 
