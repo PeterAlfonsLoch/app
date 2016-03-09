1d1694b3ceb941ed3412fb72edf2b5fcd7c8aba4
cecyn1 <cecyn1@hotmail.com>
2016-03-09 01:20:05 +0000

    LCTV contribution!! Mummi contribution (for infinities...) !! catherinacortega contribution (for weeks)!! danielv contribution!! xmetrix streamer and chat contribution (long time and this morning long)!! dchem chat contribution!! figjeti and others pals (contribution...) contribution...!! r00tkiller contribution!! humayuns, kirchik and milanj... (chat presence contribution..) contribution!! daratho, xzvf and zarytskiy... (chat presence contribution..) contribution!! denart, lugueiro and weekaa chat contribution!! danielv contribution!!
    Thank you Carlos!!
    
    Thank you Mummi!!
    
    Thank you Carol!!
    
    from inha-macos!!
    i7 2600 16GB 1333MHz DDR3 2x500GB HDD
    at /macos/ folder at inha-macos at veriverse!!

--- appseed/aura/aura/os/macos/macos_time.cpp
+++ appseed/aura/aura/os/macos/macos_time.cpp
@@ -4,49 +4,19 @@
 // http://stackoverflow.com/questions/5167269/clock-gettime-alternative-in-mac-os-x
 // http://stackoverflow.com/users/346736/jbenet
 
-//
-//inline DWORD GetTickCount()
-//{
-//   timeval ts;
-//   gettimeofday(&ts,0);
-//   return (ts.tv_sec * 1000 + (ts.tv_usec / 1000)) % 0xffffffffu;
-//
-//}
-//
-//
-//
-//DWORD get_tick_count()
-//{
-//
-//   return (DWORD) GetTickCount();
-//
-//}
-//
-//
-//
-//
-//
-//
-//
 #include <mach/clock.h>
 #include <mach/mach.h>
-//
-//
 
 uint64_t get_nanos()
 {
-    
-    mach_timespec ts;
     
     clock_serv_t cclock;
     mach_timespec_t mts;
     host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
     clock_get_time(cclock, &mts);
     mach_port_deallocate(mach_task_self(), cclock);
-    ts.tv_sec = mts.tv_sec;
-    ts.tv_nsec = mts.tv_nsec;
-    
-    return (uint64_t) ts.tv_sec * 1000 * 1000 * 1000 + (uint64_t) ts.tv_nsec;
+   
+    return ((uint64_t) mts.tv_sec * (uint64_t)1000 * (uint64_t)1000 * (uint64_t)1000 )+ ((uint64_t) mts.tv_nsec);
     
 }
 
