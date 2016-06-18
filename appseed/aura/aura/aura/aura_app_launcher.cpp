e33b221d67d00f7c5c73928637495bd6ce9160c3
camilo <camilo@ca2.email>
2016-06-18 01:29:35 +0000

    YEY! From bilbo-macos!! bilbo and Mummi contribution!! veriverse contribution!! LCTV contribution!! catherinacortega, fuel, r00tkiller, hiiamgosu, phanxgames, intelijens and zzzel  contribution!! georgeb, kaeris, kranalex, kissloryshy, epryka, vapaamies and pablosj contribution!! olivert contribution (suggestion of chat bot freelancing for gamers) and active presence contribution...!!
    Thank you Carlos!!
    
    Thank you Mummi!!
    
    from bilbo-OS X!!
    i7 6700 32 GB RAM XPS 8900

--- appseed/aura/aura/aura/aura_app_launcher.cpp
+++ appseed/aura/aura/aura/aura_app_launcher.cpp
@@ -11,11 +11,20 @@ namespace aura
    {
 
 #ifdef MACOS
+      
+      ::file::path pathHome = getenv("HOME");
+      
+      if(pathHome.is_empty())
+      {
+         
+         pathHome = "~";
+         
+      }
 
-      if(file_exists_dup("/Users/carlos/.ca2/mypath/"+m_strApp+".txt"))
+      if(file_exists_dup(pathHome / ".ca2/mypath"/m_strApp+".txt"))
       {
 
-         string strPath = file_as_string_dup("/Users/carlos/.ca2/mypath/"+m_strApp+".txt");
+         string strPath = file_as_string_dup(pathHome / ".ca2/mypath"/m_strApp+".txt");
 
          if(strPath.has_char())
          {
@@ -27,7 +36,7 @@ namespace aura
       }
       else
       {
-         file_put_contents_dup("/Users/carlos/.ca2/mypath/tst.txt", "/Users/carlos/.ca2/mypath/"+m_strApp+".txt");
+         file_put_contents_dup(pathHome / ".ca2/mypath/tst.txt", pathHome / ".ca2/mypath"+m_strApp+".txt");
       }
 
       return Sys(get_thread_app()).dir().ca2module() / "app";
