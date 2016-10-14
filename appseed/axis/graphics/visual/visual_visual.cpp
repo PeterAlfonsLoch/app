5bac1cb6e39065748cea0aaf266c274d78eec279
camilo <camilo@ca2.email>
2016-10-14 21:49:31 +0000

    Mummi and bilbo contribution!! veriverse contribution!! LCTV contribution!! catherinacortega, sjpablo, alexcode, blackhatisme, chrifrachid, euphemisminc, intelijens (Michael Bui), LaffayetteBrittoPhoto (streamer with its C300 photo), kissloryshy magrib, marianyc, michaeljcalkins, paezrice, purpleizlegitx, silvaesouza, systemis thiagovgamg, wanket contribution!! jiangyubo530, kozmo, contribution!! Twitch contribution!! AlfredoPlays (and his mates), bretterer, cpmgamingltd, Handmadehero (Casey Muratori), michaeljcalkins, nothings, Tectuma (GothicatCat1(Stacie), Tectuma(Kevin), Matty and Dylan)  contribution!! bisba contribution!! caro contribution!!
    bilbo at veriverse
    i7 6700 32GB RAM Windows 10 Pro

--- appseed/axis/graphics/visual/visual_visual.cpp
+++ appseed/axis/graphics/visual/visual_visual.cpp
@@ -439,73 +439,24 @@ namespace visual
 
       }
 
-
-      int iR = MAX(MAX(iSpreadRadius, iBlurRadius), iBlur) + 1;
-
-      ::rect rectEmboss = *lpcrect;
-
-      rectEmboss.left -= iR;
-      rectEmboss.top -= iR;
-      rectEmboss.right += iR * 3;
-      rectEmboss.bottom += iR * 3;
-
-      if (bUpdate || dib2.is_null() || dib2->area() <= 0)
+      auto pred = [&](rect rectCache, ::draw2d::graphics * pgraphics)
       {
 
-         int iEffectiveSpreadRadius = iSpreadRadius;
+         pgraphics->SelectObject(pfont);
+         pgraphics->_DrawText(strText, rectCache, iDrawTextFlags);
 
-         int iEffectiveBlurRadius = iBlurRadius;
+      };
 
-         if (dib2.is_null())
-         {
-
-            dib2.alloc(allocer());
-
-         }
-
-         class size size = rectEmboss.size();
-
-         dib2.initialize(size, iEffectiveBlurRadius);
-
-         dib2->Fill(0, 0, 0, 0);
-
-         class rect rectCache;
-
-         rectCache.left = iR + (iR >= 2 ? 1 : 0);
-         rectCache.top = iR + 1;
-         rectCache.right = rectCache.left + ::width(lpcrect);
-         rectCache.bottom = rectCache.top + ::height(lpcrect);
-
-         ::draw2d::dib_sp dib(allocer());
-
-         dib->create(size.cx, size.cy);
-         dib->Fill(0, 0, 0, 0);
-
-         ::draw2d::brush_sp brushText(allocer());
-
-         brushText->create_solid(ARGB(255, 255, 255, 255));
-         dib->get_graphics()->SelectObject(brushText);
-
-         dib->get_graphics()->SelectObject(pfont);
-         dib->get_graphics()->_DrawText(strText, rectCache, iDrawTextFlags);
-
-         System.visual().imaging().channel_spread_set_color(dib2->get_graphics(), null_point(), size, dib->get_graphics(), null_point(), ::visual::rgba::channel_alpha, iEffectiveSpreadRadius, ARGB(255, 255, 255, 255));
-
-         for (index i = 0; i < iBlur; i++)
-         {
-
-            dib2.blur();
-
-         }
-
-         dib2->set_rgb(crGlow);
-
-         dib2->mult_alpha();
-
-      }
-
-      pgraphics->set_alpha_mode(::draw2d::alpha_mode_blend);
-      pgraphics->BitBlt(rectEmboss, dib2->get_graphics());
+      bool bRet = emboss_pred(
+         pgraphics,
+         lpcrect,
+         pred,
+         dib2,
+         crGlow,
+         iSpreadRadius,
+         iBlurRadius,
+         iBlur,
+         bUpdate);
 
       ::draw2d::brush_sp brushText(allocer());
       brushText->create_solid(crText);
