9cf2262f0cbc165f3c9f442ef7724a52dae9c7d9
camilo <camilo@ca2.email>
2017-02-25 14:29:35 +0000

    Mummi and bilbo contribution!! Curitiba contribution!! veriverse contribution!! LCTV contribution!! catherinacortega, sjpablo (Pawel Kulesza), fuel (Benjamin Kuhl), r00tkiller (Alexandre),  vapaamies (Vladislav Javadov), epryka (Vova - Vladimir), theaifam5 (Mateus), doraemon (currently... also gosu - hiiamgosu,  dum - noticemesenpaiiamdum..., litAsHellhahah, icarryyourcurry), phanxgames (Henry Price), mushmouth (Patrick McFarling),  sonnystar (sonnystaminecraft, Vlad Ionescu), drmjg, 7ru57, abbyyyyy, aleksy, alexcode, andrevdb, anita, ayvatan, b3n0it, beee, bigboom, bill_gates, blackburn1911, blackhatisme, bmw, boza, breitembach, camwhite (Cameron White - YOSHICREW | ng2/node + mixing fire, Yoshicrew.com-out-loud contribution...), chrifrachid, code4life, codeotea, cookiesliyr, chubz, cubor, danielv, daratho, darkevil_s, david_joffe, denvercowboy, dgazail, diagram, dimer, dopedopedope, dzetko, dzhekson6000, elina, euphemisminc, fauker, felix22, ffabiorj, flowdeep, fork, frankly444, georgeb, gillie, gzj71ck4elm2, hasanakyildiz, heuy_freeman, hudwan, igsky, intelijens (Michael Bui), irondragon000, ishaan007lucky, jamesxism, jarzi, jaycee, jenny0030, jeraqiv, jilizart, johnyblaze, jookeer, jrtruji, kaeris, karenkododyan, karrmarr, kaznowski, kimliksiz, kissloryshy, koen, kozmo, kranalex, LaffayetteBrittoPhoto (streamer with its C300 photo), livecodingtv..., livetester, lukey_reborn78, magrib, marianyc, matroskinb, michaeljcalkins, michaelphp, milurabbit, mintisbad, monicaa, newton1, nuke, paezrice, peter_cados, patrick1, pavankataria, pudottapommin, purpleizlegitx, raymond2, rex2, rooney2, samuraijackdavidson, sanyakozmey, silvaesouza, siox, skycis, skyfoxx, smart6307 (thunder will/canal2048), sova, specter, systemis, szy, tarikogut, thiagovgamg, tonycode, trello, usandy, v0n3x, wanket, wapaca, web2ls, yanaseattle, zaaz, zakaria98, zann, zarytskiy, zeljko16 and zzzel (Marko Zel)  contribution!! jiangyubo530, kozmo, contribution!! Twitch contribution!! indaFunnyGames (Greg, Colin, Tim, Nick and Kevin contribution..), AlfredoPlays (and his mates and Fwends playing Siege: Above_Perfection, koopz, Macky43 ... ), Tectuma (GothicatCat1(Stacie), Tectuma(Kevin), Matty and Dylan), 2016pocket, 4737, BaineGames, Bialyorzeu, cary205, cpmgamingltd, Daratho, esmc103, FullNerd33, GameDevCompany (Björn Swedish-Speaking-Finlandian), GrubbyJuicde, Handmadehero (Casey Muratori), HardlyDifficult, jhovgaard, Kycklingar, kimliksiz, Letorusii, Mattyplays6, michaeljcalkins, Niabi226, nothings2 (Sean Barrett), Ojunbamo, pixel_maniacs, Prafax, Pukateiubeste, seekaplayer, Shathan, Snipe0803, sundz98 (from Sweden), talus7, tophergates, Vjzelynxykat, Wheeze202 (and hosting grumbul (British accent?)) and XanderGamingTwich (Xander Jenkins) contribution!! Beam.pro contribution!! h1dd3n, RoxenRox and Xellar contribution!! bisba contribution!! Carol, Lúcio and Lucinho contribution!! Santos contribution!!  Paulo Picciani contribution!!
    bilbo at veriverse
    i7 6700 32GB RAM Windows 10 Pro
    (booting also macOS Sierra, Hyper-V and Ubuntu 16.04 Unity Numix Theme Numix Circle Icons)

---
+++ appseed/axis/axis/compress/compress_department.cpp
@@ -0,0 +1,338 @@
+//#include"framework.h"
+//#include"axis/compress/compress.h"
+#include "compress_gzip.h"
+#include "compress_bzip.h"
+
+
+namespace axis
+{
+
+   compress_department::compress_department(::aura::application * papp) :
+      ::object(papp),
+      ::aura::department(papp)
+   {
+
+
+   }
+
+   bool compress_department::ungz(::aura::application * papp, ::file::ostream & ostreamUncompressed, const ::file::path & lpcszGzFileCompressed)
+   {
+
+      uncompress_gz ungz(papp);
+
+      return App(papp).file().output(ostreamUncompressed, &ungz, &::uncompress_gz::transfer, lpcszGzFileCompressed);
+      
+   }
+
+
+
+
+
+
+   bool compress_department::ungz(::aura::application * papp, ::file::file * pfileOut, ::file::file * pfileIn)
+   {
+
+      uncompress_gz ungz(papp);
+
+      return App(papp).file().output(pfileOut, &ungz, &::uncompress_gz::transfer, pfileIn);
+
+   }
+
+
+   bool compress_department::ungz(::aura::application * papp, ::primitive::memory_base & memOut, ::primitive::memory_base & memIn)
+   {
+
+      memory_file fileOut(papp, &memOut);
+
+      memory_file fileIn(papp, &memIn);
+
+      return ungz(papp, &fileOut, &fileIn);
+
+   }
+
+
+   bool compress_department::ungz(::aura::application * papp, ::primitive::memory_base & mem)
+   {
+
+      memory memOut;
+
+      if (!ungz(papp, memOut, mem))
+      {
+
+         return false;
+
+      }
+
+      mem = memOut;
+
+      return true;
+
+   }
+
+
+   bool compress_department::gz(::aura::application * papp, ::file::ostream & ostreamCompressed, const ::file::path & lpcszUncompressed, int iLevel)
+   {
+
+      compress_gz gz(papp, iLevel);
+
+      return App(papp).file().output(ostreamCompressed, &gz, &compress_gz::transfer, lpcszUncompressed);
+
+   }
+
+   bool compress_department::gz(::aura::application * papp, ::file::file * pfileOut, const ::file::path & lpcszUncompressed, int iLevel)
+   {
+
+      compress_gz gz(papp, iLevel);
+
+      return App(papp).file().output(pfileOut, &gz, &compress_gz::transfer, lpcszUncompressed);
+
+
+   }
+
+
+   bool compress_department::gz(::aura::application * papp, ::file::file * pfileOut, ::file::file * pfileIn, int iLevel)
+   {
+
+      compress_gz gz(papp, iLevel);
+
+      return App(papp).file().output(pfileOut, &gz, &compress_gz::transfer, pfileIn);
+
+
+   }
+
+
+
+   bool compress_department::ungz(::aura::application * papp, const ::file::path & lpcszUncompressed, const ::file::path & lpcszGzFileCompressed)
+   {
+
+      uncompress_gz ungz(papp);
+
+      return App(papp).file().output(lpcszUncompressed, &ungz, &::uncompress_gz::transfer, lpcszGzFileCompressed);
+
+   }
+
+
+
+   bool compress_department::gz(::aura::application * papp, const ::file::path & lpcszGzFileCompressed, const ::file::path & lpcszUncompressed, int iLevel)
+   {
+
+      compress_gz gz(papp, iLevel);
+
+      return App(papp).file().output(lpcszGzFileCompressed, &gz, &compress_gz::transfer, lpcszUncompressed);
+
+   }
+
+
+   bool compress_department::unbz(::aura::application * papp, ::file::ostream & ostreamUncompressed, const ::file::path & lpcszBzFileCompressed)
+   {
+
+      uncompress_bz unbz(papp);
+
+      return App(papp).file().output(ostreamUncompressed, &unbz, &::uncompress_bz::transfer, lpcszBzFileCompressed);
+
+   }
+
+
+   bool compress_department::bz(::aura::application * papp, ::file::ostream & ostreamBzFileCompressed, const ::file::path & lpcszUncompressed, int iBlockSize, int iVerbosity, int iWorkFactor)
+   {
+
+      compress_bz bz(papp, iBlockSize, iVerbosity, iWorkFactor);
+
+      return App(papp).file().output(ostreamBzFileCompressed, &bz, &::compress_bz::transfer, lpcszUncompressed);
+
+   }
+
+
+   bool compress_department::bz(::aura::application * papp, const ::file::path & lpcszBzFileCompressed, const ::file::path & lpcszUncompressed, int iBlockSize, int iVerbosity, int iWorkFactor)
+   {
+
+      compress_bz bz(papp, iBlockSize, iVerbosity, iWorkFactor);
+
+      return App(papp).file().output(lpcszBzFileCompressed, &bz, &::compress_bz::transfer, lpcszUncompressed);
+
+   }
+
+
+   bool compress_department::bz(::aura::application * papp, ::file::ostream & ostreamBzFileCompressed, ::file::istream & istreamUncompreseed, int iBlockSize, int iVerbosity, int iWorkFactor)
+   {
+
+      compress_bz bz(papp, iBlockSize, iVerbosity, iWorkFactor);
+
+      return App(papp).file().output(ostreamBzFileCompressed, &bz, &::compress_bz::transfer, istreamUncompreseed);
+
+   }
+
+
+   bool compress_department::unbz(::aura::application * papp, const ::file::path & lpcszUncompressed, const ::file::path & lpcszGzFileCompressed)
+   {
+
+      uncompress_bz unbz(papp);
+
+      return App(papp).file().output(lpcszUncompressed, &unbz, &::uncompress_bz::transfer, lpcszGzFileCompressed);
+
+
+   }
+
+
+   bool compress_department::compress(class memory & memory, void * pdata, memory_size_t ulSize)
+   {
+      memory.allocate(compressBound((uLong)ulSize) * 2);
+      uLongf ulDestSize = (uLongf)memory.get_size();
+      int32_t i = ::compress(memory.get_data(), &ulDestSize, (BYTE *)pdata, (uLongf)ulSize);
+      memory.allocate(ulDestSize);
+      return i == Z_OK;
+   }
+
+   bool compress_department::uncompress(memory & memoryUncompressed, memory & memoryCompressed, memory_size_t sizeUncompressed)
+   {
+      memoryUncompressed.allocate(sizeUncompressed);
+      uLongf ulSizeUncompressed = (uLongf)sizeUncompressed;
+      int32_t i = ::uncompress(memoryUncompressed.get_data(), &ulSizeUncompressed, memoryCompressed.get_data(), (uLong)memoryCompressed.get_size());
+      return i == Z_OK;
+   }
+
+   bool compress_department::uncompress(memory & dest, memory & memory)
+   {
+
+      int iRate = 1;
+      while (true)
+      {
+
+         dest.allocate(memory.get_size() * 10);
+
+         uLong l = dest.get_size();
+
+         int i = ::uncompress(dest.get_data(), &l, memory.get_data(), memory.get_size());
+
+         if (i == Z_OK)
+         {
+
+            dest.allocate(l);
+          
+            return true;
+
+         }
+
+         if (i == Z_BUF_ERROR)
+         {
+            iRate++;
+            if (iRate > 4)
+               return false;
+            dest.allocate(dest.get_size() * 2);
+            continue;
+         }
+
+         return false;
+
+      }
+
+   }
+
+   void compress_department::extract_all(const ::file::path & pszFile, ::aura::application * papp)
+   {
+
+      string strDir = pszFile;
+
+      ::str::ends_eat_ci(strDir, ".zip");
+
+      Sess(papp).file().copy(strDir, pszFile, false);
+
+   }
+
+
+   bool compress_department::zip(::aura::application * papp, const ::file::path & pszZip, const ::file::path & psz)
+   {
+
+      thisstart;
+
+      zip::InFile infile(papp);
+
+      if (!infile.zip_open(pszZip, 0))
+      {
+
+         thiserr << "Could not open zip file";
+
+         return false;
+
+      }
+
+      if (System.dir().is(psz, papp))
+      {
+
+         ::file::listing patha(papp);
+
+         ::file::path strPath;
+
+         ::file::file_sp file;
+
+         patha.rls_file(psz);
+
+         for (int32_t i = 0; i < patha.get_size(); i++)
+         {
+
+            infile.add_file(psz, patha[i].relative());
+
+         }
+
+      }
+
+      return true;
+
+   }
+
+
+   bool compress_department::zip(::aura::application * papp, const ::file::path & psz)
+   {
+
+      ::file::path pathNoExt;
+
+      pathNoExt = psz.folder() / psz.title();
+
+      ::file::path pathZipFileCompressed;
+
+      pathZipFileCompressed = pathNoExt + ".zip";
+
+      index i = 0;
+
+      while (true)
+      {
+
+         if (!App(papp).file().exists(pathZipFileCompressed))
+         {
+
+            break;
+
+         }
+
+         pathZipFileCompressed = pathNoExt + " (" + ::str::from(i) + ").zip";
+
+      }
+
+      return zip(papp, pathZipFileCompressed, psz);
+
+
+   }
+
+
+   bool compress_department::unzip(::aura::application * papp, const ::file::path & pathUncompressed, const ::file::path & pathZipFileCompressed)
+   {
+      
+      if (!zip::Util().extract(get_app(), pathUncompressed, pathZipFileCompressed))
+      {
+
+         return false;
+
+      }
+
+      return true;
+
+   }
+   
+
+
+} // namespace core
+
+
+
+
+
