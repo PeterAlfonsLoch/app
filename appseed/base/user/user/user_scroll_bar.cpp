fb27c5aa67f23062a17639047ccf438ade366e59
camilo <camilo@ca2.email>
2017-04-25 18:44:06 +0000

    Mummi and bilbo contribution!! veriverse contribution!! LCTV contribution!! catherinacortega, sjpablo (Pawel Kulesza), fuel (Benjamin Kuhl), r00tkiller (Alexandre),  vapaamies (Vladislav Javadov), epryka (Vova - Vladimir), theaifam5 (Mateus), doraemon (currently... also gosu - hiiamgosu, dum - noticemesenpaiiamdum..., litAsHellhahah),  phanxgames (Henry Price), mushmouth (Patrick McFarling),  sonnystar (sonnystaminecraft, Vlad Ionescu), pavankataria (Pavan Kataria), ThomasBS (Thomas Boregaard Sorensen), drmjg, 7ry57, abbyyyyy, aleksy, alexcode, andrevdb, anita, antares101, ayvatan, bazz3l, b3n0it, bigboom, blackburn1911, blackhatisme, boza, camwhite (Cameron White - YOSHICREW | ng2/node + mixing fire, Yoshicrew.com-out-loud contribution...), chrifrachid, code4life, codeotea, cookiesliyr, chubz, cubor, danielv, david_joffe, davids81, denvercowboy, diagram, didaum, dopedopedope, dzetko, dzhekson6000, euphemisminc, fauker, ffabiorj, fiote, flythiago, georgeb, gillie, goke45, gzj71ck4elm2, hailviral1 (Michael Lynn), hejsan, hudwan, igsky, intelijens (Michael Bui), ishaan007lucky, jacobs,  jarzi, jessy, jilizart, johannesdients, johnyblaze, jookeer, jrtruji, kaeris, karrmarr, kaznowski, kiiyaerick, kissloryshy, kozmo, kranalex, LaffayetteBrittoPhoto (streamer with its C300 photo), magrib, marianyc, matroskinb, michaeljcalkins, michaelphp, monicaa,  nuke, ofin, paezrice, patrick1, peter_cados, Phenotype3D, phpreject, pkshetelie, purpleizlegitx, rooney2, sanyakozmey, savitar, sdwdewwe, silvaesouza, siox, simplyronan, skyfoxx, smart6307 (thunder will/canal2048), sova, systemis, tarikogut, teras, thiagovgamg, trello, usandy, v0n3x, wanket, wapaca, web2ls, yanaseattle, zaaz and zzzel (Marko Zel)  contribution!! jiangyubo530, kozmo, contribution!! Twitch contribution!! AlfredoPlays (and his mates: Above_Perfection, Macky43, Hanson ... ), LettuceSmokeStudios (SoulFoam contribuion), Tectuma (GothicatCat1(Stacie), Tectuma(Kevin), Matty and Dylan), 88spark88, Alexetmartin,  Alex_sn69, Bialyorzeu, cary205, cpmgamingltd, Drekryan,esmc103, Feather1231, GameDevCompany (Björn, Company of Dev of Game...), GrubbyJuicde, I_am_zujul, jhovgaard (Startup Company, and mates TupperBox), Handmadehero (Casey Muratori), HardlyDifficult, Karaso00, Letorusii, Mattyplays6, michaeljcalkins, nothings2 (Sean Barrett), r00tkiller, Pyraxx, Roo220, Shathan, Skintrader_pl, Snipe0803, theD4T4, Tragonni and xandergamingtwich contribution!! Beam.pro and Microsoft contribution!! R4ver, AlexRyan21, billy_man86, boecio, Chazmaz01, cheese_burgers3, cleverZ8,  CrazyCruzNZ, FadedAce13, INFLUENTSIZ1574, Its_Calhoot, King_Ben_2nd, kingfox12175477, KRAAZYxCRUSAD3R, LuckySUB_ZERO99, Mackbar2006, Nuke_the_Cat, PoshKayan13, RIPManny21785, RoxenRox, Seanjay1128, VaguestBump, WeedyGOLDTHUG and Xellar contribution!! bisba contribution!! caro  and lucinho contribution!! Upwork contribution!! Denis Lakis contribution (Twitter Automator project contribution)!!
    bilbo at veriverse
    i7 6700 32GB RAM Windows 10 Pro 2 x 240 GB SSD 2 TB HDD
    (booting also macOS Sierra, Hyper-V and Ubuntu 16.04 Unity Numix Theme Numix Circle Icons)

--- appseed/base/user/user/user_scroll_bar.cpp
+++ appseed/base/user/user/user_scroll_bar.cpp
@@ -25,7 +25,7 @@ namespace user
    void scroll_bar::send_scroll_message(UINT nSBCode)
    {
 
-      ::message::scroll * pscroll = canew(::message::scroll(get_app()));
+      sp(::message::scroll) pscroll = canew(::message::scroll(get_app()));
 
       pscroll->m_nSBCode = nSBCode;
 
