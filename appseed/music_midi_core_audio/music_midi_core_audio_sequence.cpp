679c7f31678b231925c358024a2ba7c8aa2dcb05
camilo <camilo@ca2.email>
2017-05-01 15:44:17 +0000

    Mummi and bilbo contribution!! veriverse contribution!! LCTV contribution!! CatherinaHelena (catherinacortega), sjpablo (Pawel Kulesza), fuel (Benjamin Kuhl), r00tkiller (Alexandre),  vapaamies (Vladislav Javadov), epryka (Vova - Vladimir), theaifam5 (Mateus), doraemon (currently... also gosu - hiiamgosu, dum - noticemesenpaiiamdum..., litAsHellhahah), mushmouth (Patrick McFarling),  sonnystar (sonnystaminecraft, Vlad Ionescu), pavankataria (Pavan Kataria), phanxgames (Henry Price), drmjg, abbyyyyy, adelive, alexcode, anita, ayvatan, bigboom, blackhatisme, boza, camwhite (Cameron White - YOSHICREW | ng2/node + mixing fire, Yoshicrew.com-out-loud contribution...), chrifrachid, codeotea, cookiesliyr, chubz, cubor, danielv, denvercowboy, diagram, dopedopedope, dslegends, dzetko, dzhekson6000, ederangelo21..., elina, euphemisminc, fauker, felix22, ffabiorj, gaishnik, georgeb, gillie, gzj71ck4elm2, hailviral1 (Michael Lynn), hudwan, intelijens (Michael Bui), isabella, jarzi, jasonial, jilizart, johnyblaze, jookeer, joshsimon, kaeris, karrmarr, kaznowski, kissloryshy, kranalex, LaffayetteBrittoPhoto (streamer with its C300 photo), liveedu_support, magrib, marianyc, matroskinb, michaeljcalkins, michaelphp, novicehere, nuke, ofin, paezrice, peter_cados, PeterSwaft, pirateofthesea, purpleizlegitx, rex2, rooney2, silvaesouza, siox, smart6307 (thunder will/canal2048), sova, stevef, systemis, thiagovgamg, tonycode, usandy, v0n3x, wanket, wapaca, web2ls, yanaseattle, zaaz, zakaria98 and zzzel (Marko Zel)  contribution!! jiangyubo530, kozmo, contribution!! Twitch contribution!! AlfredoPlays (and his mates, Hanson, mackiee), Tectuma (GothicatCat1(Stacie), Tectuma(Kevin), Matty and Dylan), GameDevCompany (Björn), Handmadehero (Casey Muratori), Above_Perfection, Andreoudede, Antoxashow, Bialyorzeu, cary205, cpmgamingltd, Faegwent, Farmpy, GrubbyJuicde, HardlyDifficult, helenawinter, jhovgaard, Mattyplays6, michaeljcalkins, nothings2 (Sean Barrett), Shathan and Snipe0803 contribution!! Beam.pro contribution!! JoshiBoy667, RoxenRox and Xellar contribution!! bisba contribution!! bisba contribution!! Carol, Lúcio and Lucinho contribution!! Upwork contribution!! Denis Lakic contribution (Twitter Automator contribution)!!
    bilbo at veriverse
    i7 6700 32GB RAM 2 x 240GB SSD 2TB HDD macOS Sierra
    (booting also  Windows 10 Pro/Hyper-V and Ubuntu 16.04.1 Unity Numix Theme Numix Circle Icons)

--- appseed/music_midi_core_audio/music_midi_core_audio_sequence.cpp
+++ appseed/music_midi_core_audio/music_midi_core_audio_sequence.cpp
@@ -743,7 +743,7 @@ namespace music
          
          m_flags.unsignalize(FlagEOF);
          
-         file()->GetFlags().unsignalize(::music::midi::file::EndOfFile);
+         file()->m_ulFlags &= ~::music::midi::file::EndOfFile;
          
          
          /*
